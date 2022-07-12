517e86c37df289d76da617f2749b59b1e16e887b
Andrew Nemtsev <andrew.nemtsev@gmail.com>
2022-07-12 18:27:15 +0400

    min iOS version UP

--- Package.swift
+++ Package.swift
@@ -4,6 +4,10 @@ import PackageDescription
 
 let package = Package(
     name: "SignalRSwift",
+    platforms: [.macOS(.v10_12),
+                .iOS(.v10),
+                .tvOS(.v10),
+                .watchOS(.v3)],
     products: [
 	.library(name: "SignalRSwift", targets: ["SignalRSwift"])
     ],
